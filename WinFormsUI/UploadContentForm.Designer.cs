c3137364a1c703520bf50d7ebd34837717cfac1a
halitkalayci <halitenes.kalayci@gmail.com>
2022-11-03 11:36:40 +0300

    3.11.22

---
+++ WinFormsUI/UploadContentForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WinFormsUI
+{
+    partial class UploadContentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectBtn = new System.Windows.Forms.Button();
+            this.uploadBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectBtn
+            // 
+            this.selectBtn.Location = new System.Drawing.Point(478, 219);
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectBtn.TabIndex = 1;
+            this.selectBtn.Text = "Select";
+            this.selectBtn.UseVisualStyleBackColor = true;
+            this.selectBtn.Click += new System.EventHandler(this.selectBtn_Click);
+            // 
+            // uploadBtn
+            // 
+            this.uploadBtn.Location = new System.Drawing.Point(567, 219);
+            this.uploadBtn.Name = "uploadBtn";
+            this.uploadBtn.Size = new System.Drawing.Size(75, 23);
+            this.uploadBtn.TabIndex = 2;
+            this.uploadBtn.Text = "Upload";
+            this.uploadBtn.UseVisualStyleBackColor = true;
+            this.uploadBtn.Click += new System.EventHandler(this.uploadBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(639, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 267);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(639, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // UploadContentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 316);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.uploadBtn);
+            this.Controls.Add(this.selectBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UploadContentForm";
+            this.Text = "UploadContentForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button selectBtn;
+        private System.Windows.Forms.Button uploadBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ WinFormsUI/UploadContentForm.cs
@@ -0,0 +1,85 @@
+﻿using Business.Constants;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsUI
+{
+    public partial class UploadContentForm : Form
+    {
+        string filePath;
+        public UploadContentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void selectBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image | *.png;*.jpg";
+            DialogResult result = fileDialog.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(fileDialog.FileName);
+                filePath = fileDialog.FileName;
+            }
+        }
+
+        private void uploadBtn_Click(object sender, EventArgs e)
+        {
+            selectBtn.Enabled = false;
+            uploadBtn.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void DoUpload()
+        {
+            string cloudName = ConfigurationManager
+                .AppSettings[ConfigKeyContants.CloudinaryCloudName];
+
+            string apiKey = ConfigurationManager
+                .AppSettings[ConfigKeyContants.CloudinaryApiKey];
+
+            string apiSecret = ConfigurationManager
+                .AppSettings[ConfigKeyContants.CloudinaryApiSecret];
+
+
+            Account cloudinaryAccount = new Account(cloudName,apiKey,apiSecret);
+
+            Cloudinary cloudinary = new Cloudinary(cloudinaryAccount);
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                ImageUploadParams imageUploadParams = new ImageUploadParams();
+                imageUploadParams.File = new FileDescription(filePath);
+                imageUploadParams.UseFilename = true;
+                imageUploadParams.UseFilenameAsDisplayName = true;
+                cloudinary.Upload(imageUploadParams);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DoUpload();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Dosya yüklemesi başarılı!");
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+    }
+}
